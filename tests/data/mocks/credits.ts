d71c31daed44f7d47d723c73e9e6de497dc92586
samuelhfalci <samuelhfalci@gmail.com>
2021-04-29 16:09:08 -0300

    testing deploy

--- tests/data/mocks/credits.ts
+++ tests/data/mocks/credits.ts
@@ -2,7 +2,7 @@ import { IChargeCreditsModel } from '@/domain/models/charge/charge-credits'
 import { IChargeCredits } from '@/domain/usecases/charge/charge-credits'
 
 export class ChargeCreditsSpy implements IChargeCredits {
-  amount
+  amount: number
   schemaName: string
   async charge(params: IChargeCredits.Params, schemaName: string): Promise<IChargeCreditsModel> {
     this.amount = params.amount
