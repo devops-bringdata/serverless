f5423c8c9a44fab32f8713bac28a50d873127128
samuelhfalci <samuelhfalci@gmail.com>
2021-04-30 09:47:12 -0300

    removing vars

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -2,6 +2,6 @@ import 'source-map-support/register'
 import serverless from 'serverless-http'
 import app from '../app'
 app.get('/health', (_req, res) => {
-  res.json({ status: 'Healthy', vars: process.env })
+  res.json({ status: 'Healthy' })
 })
 export const handle = serverless(app)
